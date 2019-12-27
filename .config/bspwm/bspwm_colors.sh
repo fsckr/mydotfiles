a5a2af7ba154aa81163b78fd86f59910cb594992
fsckr <richard.freck@gmail.com>
2019-12-27 17:41:32 -0500

    My dotfiles

---
+++ .config/bspwm/bspwm_colors.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+bspc config focused_border_color "#811025"
+bspc config normal_border_color  "#580b1a"
---
+++ .config/wpg/wp_init.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+wpg -rs 'wallhaven-685222.jpg' 'wallhaven-685222.jpg'
